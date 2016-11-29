0b1b3548df6d8abe974e2481aa9d5c0089c87c05
FighterRay27 <554269861@qq.com>
2016-11-29 23:58:10 +0800

    Add Category Picker

---
+++ MyLocation/CategoryPickerViewController.swift
@@ -0,0 +1,133 @@
+//
+//  CategoryPickerViewController.swift
+//  MyLocations
+//
+//  Created by 张润峰 on 2016/11/29.
+//  Copyright © 2016年 FighterRay. All rights reserved.
+//
+
+import UIKit
+
+class CategoryPickerViewController: UITableViewController {
+    
+    var selectedIndexPath = IndexPath()
+    
+    let categories = [ "No Category",
+                       "Apple Store",
+                       "Bar",
+                       "Bookstore",
+                       "Club",
+                       "Grocery Store",
+                       "Historic Building",
+                       "House",
+                       "Icecream Vendor",
+                       "Landmark",
+                       "Park"]
+    
+    var selectedCategoryName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for i in 0..<categories.count {
+            if categories[i] == selectedCategoryName {
+                selectedIndexPath = IndexPath(row: i, section: 0)
+                break
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let categoryName = categories[indexPath.row]
+        cell.textLabel!.text = categoryName
+        
+        if indexPath == selectedIndexPath {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath != selectedIndexPath {
+            if let newCell = tableView.cellForRow(at: indexPath) {
+                newCell.accessoryType = .checkmark
+            }
+            if let oldCell = tableView.cellForRow(at: selectedIndexPath) {
+                oldCell.accessoryType = .none
+            }
+            selectedIndexPath = indexPath
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "PickedCategory" {
+            let cell = sender as! UITableViewCell
+            if let indexPath = tableView.indexPath(for: cell) {
+                selectedCategoryName = categories[indexPath.row]
+            }
+        }
+    }
+
+}
--- MyLocation/LocationDetailsViewController.swift
+++ MyLocation/LocationDetailsViewController.swift
@@ -28,9 +28,11 @@ class LocationDetailsViewController: UITableViewController {
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
     
+    var categoryName = "No Category"
+    
     override func viewDidLoad() {
         descriptionTextView.text = ""
-        catogaryLabel.text = ""
+        catogaryLabel.text = categoryName
         
         latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
         longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
@@ -81,5 +83,32 @@ class LocationDetailsViewController: UITableViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    // MARK: - Tabel View Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15;
+            addressLabel.bounds.origin = CGPoint(x: 10, y: 10)
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickCategory" {
+            let controller = segue.destination as! CategoryPickerViewController
+            controller.selectedCategoryName = categoryName
+        }
+    }
+    
+    @IBAction func categoryPickerDidPickCategory(_ segue: UIStoryboardSegue) {
+        let controller = segue.source as! CategoryPickerViewController
+        categoryName = controller.selectedCategoryName
+        catogaryLabel.text = categoryName
+    }
     
 }
